5aa5feee6a57f2c71d53e92e4ed57c79fcef1361
gvishu446 <gvishu446@gmail.com>
2026-07-20 20:18:15 +0530

    Updated website

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const menuToggle =
 document.querySelector(".menu-toggle");
 const menu =
@@ -32,40 +32,3 @@ topBtn.addEventListener("click", () =>{
         behavior:"smooth"
     });
 });
-
-=======
-const menuToggle =
-document.querySelector(".menu-toggle");
-const menu =
-document.querySelector(".menu");
-
-menuToggle.addEventListener("click",(e) => {
-    e.stopPropagation();
-    menu.classList.toggle("active");
-});
-menu.addEventListener("click", (e) => {
-    e.stopPropagation();
-});
-document.addEventListener("click", () =>{
-    menu.classList.remove("active");
-});
-
-const topBtn =
-document.getElementById("topBtn");
-window.addEventListener("scroll", () =>{
-
-    if(window.scrollY > 300){
-        topBtn.style.display ="block";
-    } else{
-        topBtn.style.display ="none";
-    }
-});
-
-topBtn.addEventListener("click", () =>{
-    window.scrollTo({
-        top:0,
-        behavior:"smooth"
-    });
-});
-
->>>>>>> 41c4fc7bf1ff2822fb7472f55ce53f4ad5f5d207
